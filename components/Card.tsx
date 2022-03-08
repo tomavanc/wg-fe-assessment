d1a56bfa7a03139ca2926437edd57318aac1d14e
Tomas Vancoillie <vancoillietomas@gmail.com>
2022-03-08 07:56:06 +0100

    Styling pokemon card

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,12 @@
-import { Box, Center, Heading, Image, Text, Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Heading,
+  Image,
+  Text,
+  Flex,
+  Spacer,
+} from '@chakra-ui/react';
 import { useQuery } from 'react-query';
 import { fetchPokemon } from '../api/fetchers';
 
@@ -16,46 +24,73 @@ export default function Card({ name }: { name?: string }) {
   }
 
   return (
-    <Box
-      w='100%'
-      p={5}
-      shadow='md'
-      borderWidth='1px'
-      flex='1'
-      borderRadius='md'
-    >
-      <Center>
-        <Heading as='h2' size='xl'>
-          {pokemon.name} ({pokemon.id})
-        </Heading>
-      </Center>
+    <Center w='100%'>
+      <Box
+        w='500px'
+        h='700px'
+        p={5}
+        borderWidth='15px'
+        borderColor='#ffcb05'
+        borderRadius='xl'
+        bg='#E8E8E8'
+      >
+        <Flex>
+          <Heading as='h2' size='xl'>
+            {pokemon.name}
+          </Heading>
+          <Spacer />
+          <Heading as='h2' size='xl'>
+            {pokemon.id}
+          </Heading>
+        </Flex>
 
-      <Center>
-        <Image
-          src={pokemon.sprites.front_default}
-          alt={pokemon.name}
-          boxSize='250px'
-          objectFit='cover'
-        />
-      </Center>
+        <Center
+          borderWidth='1px'
+          borderColor='lightGray'
+          my='5'
+          borderRadius='xl'
+          bg='#F8F8F8'
+        >
+          <Image
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            boxSize='250px'
+            objectFit='cover'
+            borderWidth='1px'
+          />
+        </Center>
 
-      <Center>
-        <Text fontSize='lg'>Weight {pokemon.weight}</Text>
-      </Center>
+        <Flex pb='5'>
+          <Center w='50%'>
+            <Flex direction='column' align='center'>
+              <Text>Weight</Text>
+              <Text fontSize='3xl'>{pokemon.weight}</Text>
+            </Flex>
+          </Center>
+          <Center w='50%'>
+            <Flex direction='column' align='center'>
+              <Text>Height</Text>
+              <Text fontSize='3xl'>{pokemon.height}</Text>
+            </Flex>
+          </Center>
+        </Flex>
 
-      <Center>
-        <Text fontSize='lg'>Height {pokemon.height}</Text>
-      </Center>
+        <Center py='5' borderTop='1px' borderColor='lightGray'>
+          <Text fontSize='xl'>Moves</Text>
+        </Center>
 
-      <Center py='5'>
-        <Text fontSize='xl'>Moves</Text>
-      </Center>
-
-      <Flex maxH='300px' overflow='scroll' direction='column'>
-        {pokemon.moves.map(({ move }) => (
-          <Center key={move.name}>{move.name}</Center>
-        ))}
-      </Flex>
-    </Box>
+        <Flex
+          maxH='140px'
+          overflow='scroll'
+          direction='column'
+          borderRadius='xl'
+          bg='#F0F0F0'
+        >
+          {pokemon.moves.map(({ move }) => (
+            <Center key={move.name}>{move.name}</Center>
+          ))}
+        </Flex>
+      </Box>
+    </Center>
   );
 }
